93154706812f26a4654bce56fb7e090d40b7af91
GabrielAlvao <albuquerquealves2002@hotmail.com>
2021-10-06 19:34:54 -0300

    Corrigindo codigo no lugar errado

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -15,12 +15,6 @@ export class PostagemService {
   token = {
     headers: new HttpHeaders().set('Authorization', environment.token)
   }
-  refreshToken(){
-    this.token ={
-      headers: new HttpHeaders().set('Authorization', environment.token)
-    }
-  }
-
   getAllPostagens():Observable<Postagem[]>{
     return this.http.get<Postagem[]>('https://gabriblogelpessoal.herokuapp.com/postagem', this.token)
   }
